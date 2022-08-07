41cf27ca595ba8b2a0ed22759b5eb5ae0c9367e1
HUI NIU <hui-niu@outlook.com>
2022-08-07 17:24:15 +1200

    finish the assignment

--- src/App.tsx
+++ src/App.tsx
@@ -39,68 +39,79 @@ const App: React.FunctionComponent = () => {
           </div>
         </nav>
       </div>
-      <form onSubmit={onSubmit} className="App-content">
-        <div>
-          Welcome,&nbsp;
-          {showName}
+      {sidebar === false ? (
+        <form onSubmit={onSubmit} className="App-content">
+          <div>
+            Welcome,&nbsp;
+            {showName}
+          </div>
+          <div>
+            <input
+              {...register("cardnumber", {
+                required: true,
+                minLength: 16,
+                maxLength: 16,
+                pattern: {
+                  value: /[0-9]{16}/,
+                  message: "error message",
+                },
+              })}
+              id="cardnumber"
+              name="cardnumber"
+              type="text"
+              placeholder="Credit Card Number"
+              className="input"
+              onChange={handleChange}
+            />
+            {errors.cardnumber && (
+              <p className="error-size">Required 16 Digits</p>
+            )}
+          </div>
+          <div>
+            <input
+              {...register("cvc", {
+                required: true,
+                minLength: 3,
+                maxLength: 3,
+                pattern: {
+                  value: /[0-9]{3}/,
+                  message: "error message",
+                },
+              })}
+              id="cvc"
+              name="cvc"
+              type="text"
+              placeholder="CVC"
+              className="input"
+            />
+            {errors.cvc && <p className="error-size">Required 3 Digits</p>}
+          </div>
+          <div>
+            <input
+              {...register("expiry", { required: true })}
+              id="expiry"
+              name="expiry"
+              type="date"
+              placeholder="Expiry Date"
+              className="date"
+            />
+            {errors.expiry && <p className="error-size">Select Expiry Date</p>}
+          </div>
+          <div></div>
+          <button className="submit" type="submit">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <div className="navContent">
+          <div>Accounts and Cards</div>
+          <div>Loans and Mortgages</div>
+          <div>KiwiSaver and Investments</div>
+          <div>Insurance</div>
+          <div>International and Foreign Exchange</div>
+          <div>Business and Rural</div>
         </div>
-        <div>
-          <input
-            {...register("cardnumber", {
-              required: true,
-              minLength: 16,
-              maxLength: 16,
-              pattern: {
-                value: /[0-9]{16}/,
-                message: "error message",
-              },
-            })}
-            id="cardnumber"
-            name="cardnumber"
-            type="text"
-            placeholder="Credit Card Number"
-            className="input"
-            onChange={handleChange}
-          />
-          {errors.cardnumber && (
-            <p className="error-size">Required 16 Digits</p>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("cvc", {
-              required: true,
-              minLength: 3,
-              maxLength: 3,
-              pattern: {
-                value: /[0-9]{3}/,
-                message: "error message",
-              },
-            })}
-            id="cvc"
-            name="cvc"
-            type="text"
-            placeholder="CVC"
-            className="input"
-          />
-          {errors.cvc && <p className="error-size">Required 3 Digits</p>}
-        </div>
-        <div>
-          <input
-            {...register("expiry", { required: true })}
-            id="expiry"
-            name="expiry"
-            type="date"
-            placeholder="Expiry Date"
-            className="date"
-          />
-          {errors.expiry && <p className="error-size">Select Expiry Date</p>}
-        </div>
-        <div></div>
-        <button className="submit" type="submit">
-          Submit
-        </button>
-      </form>
+      )}
     </div>
   );
 };
