94c3055142f21d81153c51a23707e769451a5413
HUI NIU <hui-niu@outlook.com>
2022-08-07 17:14:02 +1200

    add functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./App.css";
 import { AiOutlineMenu, AiOutlineArrowLeft } from "react-icons/ai";
 import { useForm } from "react-hook-form";
@@ -11,19 +11,31 @@ type Profile = {
 
 const App: React.FunctionComponent = () => {
   const [sidebar, setSidebar] = useState(false);
+  const [cardNumber, setCardNumber] = useState<string>("");
+  const [showName, setShowName] = useState<string>();
 
   const showSidebar = () => setSidebar(!sidebar);
+
+  const { handleSubmit } = useForm<Profile>();
+
+  const onSubmit = handleSubmit((data): void => {
+    setShowName(cardNumber);
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setCardNumber(event.target.value);
+  };
 
   return (
     <div className="App">
       <div>
-        <nav className="nav">
+        <nav className="nav" onClick={showSidebar}>
           <div className="navIcon" onClick={showSidebar}>
             {sidebar === false ? <AiOutlineMenu /> : <AiOutlineArrowLeft />}
           </div>
         </nav>
       </div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>Welcome,&nbsp;</div>
         <div>
           <input
@@ -32,6 +44,7 @@ const App: React.FunctionComponent = () => {
             type="text"
             placeholder="Credit Card Number"
             className="input"
+            onChange={handleChange}
           />
         </div>
         <div>
