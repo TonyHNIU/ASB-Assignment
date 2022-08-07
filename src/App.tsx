c22b70948bbf6168a8daaa5c6e810bd69e20b200
HUI NIU <hui-niu@outlook.com>
2022-08-07 17:08:20 +1200

    build layout without functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import { AiOutlineMenu, AiOutlineArrowLeft } from "react-icons/ai";
+import { useForm } from "react-hook-form";
+
+type Profile = {
+  cardnumber: number;
+  cvc: number;
+  expiry: string;
+};
 
 const App: React.FunctionComponent = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -16,6 +23,40 @@ const App: React.FunctionComponent = () => {
           </div>
         </nav>
       </div>
+      <form>
+        <div>Welcome,&nbsp;</div>
+        <div>
+          <input
+            id="cardnumber"
+            name="cardnumber"
+            type="text"
+            placeholder="Credit Card Number"
+            className="input"
+          />
+        </div>
+        <div>
+          <input
+            id="cvc"
+            name="cvc"
+            type="text"
+            placeholder="CVC"
+            className="input"
+          />
+        </div>
+        <div>
+          <input
+            id="expiry"
+            name="expiry"
+            type="date"
+            placeholder="Expiry Date"
+            className="date"
+          />
+        </div>
+        <div></div>
+        <button className="submit" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
