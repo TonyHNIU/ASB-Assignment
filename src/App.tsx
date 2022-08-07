bea55aa4396efb90ffff6ae5fb60dda72637e23d
HUI NIU <hui-niu@outlook.com>
2022-08-07 16:58:04 +1200

    add menu icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { AiOutlineMenu, AiOutlineArrowLeft } from "react-icons/ai";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FunctionComponent = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <div className="App">
+      <div>
+        <nav>
+          <div onClick={showSidebar}>
+            {sidebar === false ? <AiOutlineMenu /> : <AiOutlineArrowLeft />}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default App;
